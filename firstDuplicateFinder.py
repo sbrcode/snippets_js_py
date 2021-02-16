e15ecaf6748350f394aafabb357a52168e750453
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-16 17:42:07 +0100

    js-part-done

--- firstDuplicateFinder.py
+++ firstDuplicateFinder.py
@@ -7,15 +7,12 @@ def finder():
 
 # Here begin test part 
 
-array1 = [1, 2, 3, 3, 5, 6, 7, 8, 9, 10]
-
+array1 = [1, 2, 3, 5, 3, 6, 7, 8, 9, 10]
 array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-array3 = [1, 2, 3, 4, 5, 6, 6, 8, 4, 10]
-
-array4 = [1, 8, 3, 4, 5, 10, 5, 8, 8, 10]
+array3 = [1, 2, 3, 4, 5, 6, 7, 8, 10, 10]
+array4 = [1, 8, 3, 4, 5, 10, 5, 8, 8, 4]
 
 print(finder(array1), "(\t*4)Answer is: 3")
 print(finder(array2), "\t\t\t\tAnswer is: no duplicate")
-print(finder(array3), "\t\t\t\tAnswer is: 6")
+print(finder(array3), "\t\t\t\tAnswer is: 10")
 print(finder(array4), "\t\t\t\tAnswer is: 5")
