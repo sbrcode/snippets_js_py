cc631cfc201353fcac24d99e19d54c226616114c
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-17 18:38:11 +0100

    test-writing

--- anagramFinder.js
+++ anagramFinder.js
@@ -1,20 +1,26 @@
-/** determine if 2 strings are anagrams  */
-
-function finder(val){
-    return false
-}
-
 /**
- * Here begin test part 
+ * determine if 2 strings are anagrams
  * 
  */
 
-var value1 = "radar";
-var value2 = "radars";
-var value3 = [1, 2];
-var value4 = true;
+function finder(val1, val2){
+    if(typeof val1 === "string" && typeof val2 === "string"){
+        return true
+    }
+    return false
+}
 
-console.log(`${finder(value1)}\t\t\t\tAnagram is: true`);
-console.log(`${finder(value2)}\t\t\t\tAnagram is: false`);
-console.log(`${finder(value3)}\t\t\t\tAnagram is: false`);
-console.log(`${finder(value4)}\t\t\t\tAnagram is: false`);
+
+/** Here begin test part */
+
+var value11 = "nacre";var value12 = "rance";
+var value21 = "tenancières";var value22 = "cannetières";
+var value31 = "vaginales";var value32 = "Galvanise";
+var value41 = "cador";var value42 = "crade";
+var value51 = "jeudi";var value52 = true;
+
+console.log(`${finder(value11, value12)}\t\t\t\tAnagram? true`);
+console.log(`${finder(value21, value22)}\t\t\t\tAnagram? true`);
+console.log(`${finder(value31, value32)}\t\t\t\tAnagram? true`);
+console.log(`${finder(value41, value42)}\t\t\t\tAnagram? false`);
+console.log(`${finder(value51, value52)}\t\t\t\tAnagram? false`);
--- anagramFinder.py
+++ anagramFinder.py
@@ -2,18 +2,22 @@
 determine if 2 strings are anagrams 
 '''
 
-def finder(val):
-    pass
+def finder(val1, val2):
+    if isinstance(val1, str) and isinstance(val2, str):
+        return True
+    return False
 
 
 # Here begin test part 
 
-value1 = "radar"
-value2 = "radars"
-value3 = [1, 2]
-value4 = True
+value11 = "nacre"; value12 = "rance"
+value21 = "tenancières"; value22 = "cannetières"
+value31 = "vaginales"; value32 = "Galvanise"
+value41 = "cador"; value42 = "crade"
+value51 = "jeudi"; value52 = True
 
-print(finder(value1), "\t\t\t\tAnagram is: true")
-print(finder(value2), "\t\t\t\tAnagram is: false")
-print(finder(value3), "\t\t\t\tAnagram is: false")
-print(finder(value4), "\t\t\t\tAnagram is: false")
+print(finder(value11, value12), "\t\t\t\tAnagram? true")
+print(finder(value21, value22), "\t\t\t\tAnagram? true")
+print(finder(value31, value32), "\t\t\t\tAnagram? true")
+print(finder(value41, value42), "\t\t\t\tAnagram? false")
+print(finder(value51, value52), "\t\t\t\tAnagram? false")
