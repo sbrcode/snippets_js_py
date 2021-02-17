05942f11afc6c4fac710b67dc9c0946e050b87cb
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-17 18:11:21 +0100

    files-creation

---
+++ anagramFinder.js
@@ -0,0 +1,20 @@
+/** determine if 2 strings are anagrams  */
+
+function finder(val){
+    return false
+}
+
+/**
+ * Here begin test part 
+ * 
+ */
+
+var value1 = "radar";
+var value2 = "radars";
+var value3 = [1, 2];
+var value4 = true;
+
+console.log(`${finder(value1)}\t\t\t\tAnagram is: true`);
+console.log(`${finder(value2)}\t\t\t\tAnagram is: false`);
+console.log(`${finder(value3)}\t\t\t\tAnagram is: false`);
+console.log(`${finder(value4)}\t\t\t\tAnagram is: false`);
---
+++ anagramFinder.py
@@ -0,0 +1,19 @@
+'''
+determine if 2 strings are anagrams 
+'''
+
+def finder(val):
+    pass
+
+
+# Here begin test part 
+
+value1 = "radar"
+value2 = "radars"
+value3 = [1, 2]
+value4 = True
+
+print(finder(value1), "\t\t\t\tAnagram is: true")
+print(finder(value2), "\t\t\t\tAnagram is: false")
+print(finder(value3), "\t\t\t\tAnagram is: false")
+print(finder(value4), "\t\t\t\tAnagram is: false")
