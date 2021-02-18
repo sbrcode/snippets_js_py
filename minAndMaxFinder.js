5be035da1397785bb3a9939efd0439de1b8bdd84
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-18 17:26:12 +0100

    completed-but-not-optimized

--- minAndMaxFinder.js
+++ minAndMaxFinder.js
@@ -4,7 +4,7 @@
  */
 
 function finder(arr){
-    return false
+    return `Min: ${Math.min(...arr)} Max: ${Math.max(...arr)}`
 }
 
 /**
@@ -16,8 +16,10 @@ var array1 = [1, 2, 3, 100, 3, 6, 7, 8, 9, 10];
 var array2 = ['a', 9, 10];
 var array3 = [1, 2, 3, 4, true, 6, 7, 8, 'b', 0];
 var array4 = [8, 3.1, 4, 5.1, 11, 5, 8, 8, 4];
+var array5 = [8, -1, 4, -12, 11, 5, 8, 8, 4];
 
-console.log(`${finder(array1)}\t\t\t\tMin: 3, Max: 100`);
+console.log(`${finder(array1)}\t\t\t\tMin: 1, Max: 100`);
 console.log(`${finder(array2)}\t\t\t\tMin: 9, Max: 10`);
 console.log(`${finder(array3)}\t\t\t\tMin: 1, Max: 8`);
 console.log(`${finder(array4)}\t\t\t\tMin: 3.1, Max: 11`);
+console.log(`${finder(array5)}\t\t\t\tMin: -12, Max: 11`);
--- minAndMaxFinder.py
+++ minAndMaxFinder.py
@@ -3,7 +3,14 @@ determine min and max values of an integer array
 '''
 
 def finder(arr):
-    pass
+    arrStr = []
+    for i in arr:
+        if isinstance(i, int):
+            arrStr.append(i)
+    if len(arrStr) == 0:
+        return "min: ", min(arr), "max: ", max(arr)
+    else:
+        return "min: ", min(arrStr), "max: ", max(arrStr)
 
 
 # Here begin test part 
@@ -12,8 +19,10 @@ array1 = [1, 2, 3, 100, 3, 6, 7, 8, 9, 10]
 array2 = ['a', 9, 10]
 array3 = [1, 2, 3, 4, True, 6, 7, 8, 'b', 0]
 array4 = [8, 3.1, 4, 5.1, 11, 5, 8, 8, 4]
+array5 = [8, -1, 4, -12, 11, 5, 8, 8, 4]
 
-print(finder(array1), "\t\t\t\tMin: 3, Max: 100")
+print(finder(array1), "\t\t\t\tMin: 1, Max: 100")
 print(finder(array2), "\t\t\t\tMin: 9, Max: 10")
 print(finder(array3), "\t\t\t\tMin: 1, Max: 8")
 print(finder(array4), "\t\t\t\tMin: 3.1, Max: 11")
+print(finder(array5), "\t\t\t\tMin: -12, Max: 11")
