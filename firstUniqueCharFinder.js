214f633093ed9483acbb25531e3c64afb554e71c
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-22 11:51:30 +0100

    js-part-completed

--- firstUniqueCharFinder.js
+++ firstUniqueCharFinder.js
@@ -4,7 +4,19 @@
  */
 
 function finder(chain){
-
+    if(typeof chain === "string"){
+        chain = chain.toLowerCase();
+        for(char of chain){
+            let count = chain.split(char).length - 1;
+            if(count === 1){
+                if(char !== ' '){
+                    return char
+                }
+            }
+        }
+        return "No unique char !"
+    }
+    return "Not a String !"
 }
 
 /**
@@ -14,12 +26,12 @@ function finder(chain){
 
 var chain1 = 'du beurre de baratte';
 var chain2 = 'Laval avale et alors?';
-var chain3 = '1 Valvule';
+var chain3 = ' 1Valvule';
 var chain4 = true;
 var chain5 = ['agua', 'gwada'];
 
-console.log(`${finder(chain1)}\t\t\t\tAnswer: no unique char !`);
-console.log(`${finder(chain2)}\t\t\t\t\tAnswer: o`);
+console.log(`${finder(chain1)}\t\t\tAnswer: No unique char !`);
+console.log(`${finder(chain2)}\t\t\t\t\tAnswer: t`);
 console.log(`${finder(chain3)}\t\t\t\t\tAnswer: 1`);
 console.log(`${finder(chain4)}\t\t\t\tMot a String !`);
 console.log(`${finder(chain5)}\t\t\t\tNot a String !`);
--- firstUniqueCharFinder.py
+++ firstUniqueCharFinder.py
@@ -12,11 +12,11 @@ def finder(chain):
 chain1 = 'du beurre de baratte'
 chain2 = 'Laval avale et alors?'
 chain3 = '1 Valvule'
-chain4 = true
+chain4 = True
 chain5 = ['agua', 'gwada']
 
-print(finder(chain1), "\t\t\t\tAnswer: no unique char !")
-print(finder(chain2), "\t\t\t\t\tAnswer: o")
+print(finder(chain1), "\t\t\t\tAnswer: No unique char !")
+print(finder(chain2), "\t\t\t\t\tAnswer: t")
 print(finder(chain3), "\t\t\t\t\tAnswer: 1")
 print(finder(chain4), "\t\t\t\tMot a String !")
 print(finder(chain5), "\t\t\t\tMot a String !")
