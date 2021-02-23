ccf22bbc0a3cb1e8510db756228bccaba336566c
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-23 16:15:53 +0100

    tests-part-done

--- integerPairsFinder.js
+++ integerPairsFinder.js
@@ -3,8 +3,10 @@
  * 
  */
 
+const arr = [1, 2, 4, 100, 4, 5, 6, 7.1, 8, 9, 0.9, 0];
+
 function finder(arr){
-    
+
 }
 
 /**
@@ -12,16 +14,10 @@ function finder(arr){
  * 
  */
 
-var array1 = [1, 2, 3, 100, 3, 6, 7, 8, 9, 10];
-var array2 = ['a', 9, 10];
-var array3 = [1, 2, 3, 4, true, 6, 7, 8, 'b', 0];
-var array4 = [8, 3.1, 4, 5.1, 11, 5, 8, 8, 4];
-var array5 = [8, -1.1, 4, -12, 11, 5, 8, 8, 4];
-var array6 = ['a', false, [0, 1]];
+var const1 = 3;
+var const2 = 8;
+var const3 = 103;
 
-console.log(`${finder(array1)}\t\t\t\t\tMin: 1, Max: 100`);
-console.log(`${finder(array2)}\t\t\t\t\tMin: 9, Max: 10`);
-console.log(`${finder(array3)}\t\t\t\t\tMin: 0, Max: 8`);
-console.log(`${finder(array4)}\t\t\t\tMin: 3.1, Max: 11`);
-console.log(`${finder(array5)}\t\t\t\tMin: -12, Max: 11`);
-console.log(`${finder(array6)}\t\t\t\tNot a number array !`);
+console.log(`${finder(const1)}\t\t\t\t\tPairs are : 1 & 2`);
+console.log(`${finder(const2)}\t\t\t\t\tPairs are : 2 & 6, 4 & 4, 8 & 0`);
+console.log(`${finder(const3)}\t\t\t\t\tNo Pairs at all !`);
--- integerPairsFinder.py
+++ integerPairsFinder.py
@@ -2,21 +2,17 @@
 find all pairs of integers in an array whose sum is equal a given constant
 '''
 
+arr = [1, 2, 4, 100, 4, 5, 6, 7.1, 8, 9, 0.9, 0]
+
 def finder(arr):
     pass
 
 # Here begin test part 
 
-array1 = [1, 2, 3, 100, 3, 6, 7, 8, 9, 10]
-array2 = ['a', 9, 10]
-array3 = [1, 2, 3, 4, True, 6, 7, 8, 'b', 0]
-array4 = [8, 3.1, 4, 5.1, 11, 5, 8, 8, 4]
-array5 = [8, -1.1, 4, -12, 11, 5, 8, 8, 4]
-array6 = ['a', False, [0, 1]]
+const1 = 3
+const2 = 8
+const3 = 103
 
-print("%s\t\t\t\tMin: 1, Max: 100" % finder(array1))
-print("%s\t\t\t\t\tMin: 9, Max: 10" % finder(array2))
-print("%s\t\t\t\t\tMin: 0, Max: 8" % finder(array3))
-print("%s\t\t\t\tMin: 3.1, Max: 11" % finder(array4))
-print("%s\t\t\t\tMin: -12, Max: 11" % finder(array5))
-print("%s\t\t\t\tNot a number array !" % finder(array6))
+print("%s\t\t\t\t\tPairs are : 1 & 2" % finder(const1))
+print("%s\t\t\t\t\tPairs are : 2 & 6, 4 & 4, 8 & 0" % finder(const2))
+print("%s\t\t\t\t\tNo Pairs at all !" % finder(const3))
