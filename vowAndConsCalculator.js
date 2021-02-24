5e068364b1f67b36994d6b44ccb76147d5eaddc3
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-24 16:31:40 +0100

    test-case-operational

---
+++ vowAndConsCalculator.js
@@ -0,0 +1,23 @@
+/**
+ * calculate the number of vowels and consonants in a string
+ *
+ */
+
+function finder(chain){
+
+}
+
+/**
+ * Here begin test part 
+ * 
+ */
+
+var chain1 = 'stp';
+var chain2 = 'Nabuchodonosor';
+var chain3 = true;
+var chain4 = 'part_1';
+
+console.log(`${finder(chain1)}\t\t\t\t\t${chain1}\t\tVows: 0 - Cons: 3`);
+console.log(`${finder(chain2)}\t\t\t\t\t${chain2}\tVows: 6 - Cons: 8`);
+console.log(`${finder(chain3)}\t\t\t\t\t${chain3}\t\tis not a string input !`);
+console.log(`${finder(chain3)}\t\t\t\t\t${chain4}\t\tVows: 1 - Cons: 3 - Others: 2`);
---
+++ vowAndConsCalculator.py
@@ -0,0 +1,18 @@
+'''
+calculate the number of vowels and consonants in a string
+'''
+
+def finder(chain):
+    pass
+
+# Here begin test part 
+
+chain1 = 'stp'
+chain2 = 'Nabuchodonosor'
+chain3 = True
+chain4 = 'part_1'
+
+print("%s\t\t\t\t\t%s\t\tVows: 0 - Cons: 3" % (finder(chain1), chain1))
+print("%s\t\t\t\t\t%s\tVows: 6 - Cons: 8" % (finder(chain2), chain2))
+print("%s\t\t\t\t\t%s\t\tis not a string input !" % (finder(chain3), chain3))
+print("%s\t\t\t\t\t%s\t\tVows: 1 - Cons: 3 - Others: 2" % (finder(chain4), chain4))
