f36bb31b34c1025c6084f4a9fbf334b2ce02d3f3
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-25 17:20:45 +0100

    init-readme-files

---
+++ isPalindrome.js
@@ -0,0 +1,23 @@
+/**
+ * determine if a given string is a palindrome
+ *
+ */
+
+function finder(chain){
+
+}
+
+/**
+ * Here begin test part 
+ * 
+ */
+
+var chain1 = 'Laval';
+var chain2 = '156651';
+var chain3 = 12321;
+var chain4 = 'semâmes';
+
+console.log(`${finder(chain1)}\t\t\t\tis Palindrome !`);
+console.log(`${finder(chain2)}\t\t\t\tis Palindrome !`);
+console.log(`${finder(chain3)}\t\t\t\tis not a string input !`);
+console.log(`${finder(chain4)}\t\t\t\tis Palindrome !`);
---
+++ isPalindrome.py
@@ -0,0 +1,18 @@
+'''
+determine if a given string is a palindrome
+'''
+
+def finder(chain):
+    pass
+
+# Here begin test part 
+
+chain1 = 'Laval'
+chain2 = '156651'
+chain3 = 12321
+chain4 = 'semâmes'
+
+print("%s\t\t\t\tis Palindrome !" % finder(chain1))
+print("%s\t\t\t\tis Palindrome !" % finder(chain2))
+print("%s\t\t\t\tis not a string input !" % finder(chain3))
+print("%s\t\t\t\tis Palindrome !" % finder(chain4))
