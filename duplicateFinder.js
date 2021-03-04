f1976c848108e1efdd7d4895dd5890bd2b54190b
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-03-04 18:04:06 +0100

    js-part-ok-except-object-in-array

--- duplicateFinder.js
+++ duplicateFinder.js
@@ -1,7 +1,15 @@
 /** find all duplicate values of an array */
 
 function finder(arr){
-    return arr.filter((elem, pos) => arr.indexOf(elem) !== pos); 
+    arrF = arr.filter((elem, pos) => arr.indexOf(elem) !== pos);
+    return arrF.length !== 0 ? arrF : "No duplicate"
+}
+
+function removeDuplicity(arr){
+    return arr.filter((item, index, arr) => {
+        const c = arr.map(item => item.name);
+        return index === c.indexOf(item.name)
+    })
 }
 
 /**
@@ -9,12 +17,14 @@ function finder(arr){
  * 
  */
 
-var array1 = [1, 2, 3, 5, 6, 7, 8, 9, 10, 3];
+var array1 = [1, 2, 3, 5, 6, true, 7, 8, 9, 10, 3, true];
 var array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var array3 = [1, 4, 3, 4, 5, 6, 7, 8, 10, 10, 6];
-var array4 = [1, 8, 3, 8, 5, 10, 5, 8, 8, 4];
+var array3 = [1, 4, 3, 'a', 4, 5, -6, 7, 8, 10, 'a', 10, -6];
+var array4 = [1, 8, 3, 8, 5.1, 10, 5.1, 8, 8, 4];
+var array5 = [{"a": 1}, {"b": 2}, {"b": 1}, {"a": 1}];
 
-console.log(`${finder(array1)}\t\t\t\tAnswer is: 3`);
-console.log(`${finder(array2)}\t\t\tAnswer is: no duplicate`);
-console.log(`${finder(array3)}\t\t\t\tAnswer is: 4, 6, 10`);
-console.log(`${finder(array4)}\t\t\t\tAnswer is: 5, 8`);
+console.log(`${finder(array1)}\t\t\t\t|| 3,true`);
+console.log(`${finder(array2)}\t\t\t\t|| No duplicate`);
+console.log(`${finder(array3)}\t\t\t\t|| 4,a,10,-6`);
+console.log(`${finder(array4)}\t\t\t\t|| 8,5.1,8,8`);
+console.log(`${finder(array5)}\t\t\t\t|| {'a': 1}`);
--- duplicateFinder.py
+++ duplicateFinder.py
@@ -8,12 +8,14 @@ def finder(arr):
 
 # Here begin test part 
 
-array1 = [1, 2, 3, 5, 6, 7, 8, 9, 10, 3]
+array1 = [1, 2, 3, 5, 6, true, 7, 8, 9, 10, 3, true]
 array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array3 = [1, 4, 3, 4, 5, 6, 7, 8, 10, 10, 6]
-array4 = [1, 8, 3, 8, 5, 10, 5, 8, 8, 4]
+array3 = [1, 4, 3, 'a', 4, 5, -6, 7, 8, 10, 'a', 10, -6]
+array4 = [1, 8, 3, 8, 5.1, 10, 5.1, 8, 8, 4]
+array5 = [{"a": 1}, {"b": 2}, {"b": 1}, {"a": 1}]
 
-print("%s \t\t\t\tAnswer is: 3" % finder(array1))
-print("%s \t\t\tAnswer is: no duplicate" % finder(array2))
-print("%s \t\t\t\tAnswer is: 4, 6, 10" % finder(array3))
-print("%s \t\t\t\tAnswer is: 5, 8" % finder(array4))
+print("%s \t\t\t\t|| 3,true" % finder(array1))
+print("%s \t\t\t\t|| No duplicate" % finder(array2))
+print("%s \t\t\t\t|| 4,a,10,-6" % finder(array3))
+print("%s \t\t\t\t|| 8,5.1,8,8" % finder(array4))
+print("%s \t\t\t\t|| {'a': 1}" % finder(array5))
