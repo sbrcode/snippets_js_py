f8704d4eb85fbbd971244fe3f9097f550c999d15
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-03-23 18:06:28 +0100

    add-average

--- minAndMaxAndAverageFinder.js
+++ minAndMaxAndAverageFinder.js
@@ -13,7 +13,8 @@ function finder(arr){
     if(arrNum.length === 0){
         return "Not a number array !"
     }
-    return `Min: ${Math.min(...arrNum)} Max: ${Math.max(...arrNum)}`
+    const ave = arrNum.reduce((acc, curVal) => acc + curVal, 0) / arrNum.length;
+    return `Min: ${Math.min(...arrNum)} Max: ${Math.max(...arrNum)} Ave: ${ave}`
 }
 
 /**
@@ -28,9 +29,9 @@ var array4 = [8, 3.1, 4, 5.1, 11, 5, 8, 8, 4];
 var array5 = [8, -1.1, 4, -12, 11, 5, 8, 8, 4];
 var array6 = ['a', false, [0, 1]];
 
-console.log(`${finder(array1)}\t\t\t\t\tMin: 1, Max: 100`);
-console.log(`${finder(array2)}\t\t\t\t\tMin: 9, Max: 10`);
-console.log(`${finder(array3)}\t\t\t\t\tMin: 0, Max: 8`);
-console.log(`${finder(array4)}\t\t\t\tMin: 3.1, Max: 11`);
-console.log(`${finder(array5)}\t\t\t\tMin: -12, Max: 11`);
+console.log(`${finder(array1)}\t\t\t\t\tMin: 1, Max: 100, Ave: 14.9`);
+console.log(`${finder(array2)}\t\t\t\t\tMin: 9, Max: 10, Ave: 9.5`);
+console.log(`${finder(array3)}\t\t\t\t\tMin: 0, Max: 8, Ave: 3.875`);
+console.log(`${finder(array4)}\t\t\t\tMin: 3.1, Max: 11, Ave: 6.2444...`);
+console.log(`${finder(array5)}\t\t\t\tMin: -12, Max: 11, Ave: 3.8777...`);
 console.log(`${finder(array6)}\t\t\t\tNot a number array !`);
